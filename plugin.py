5d4070f43c01746f97058d96220b8bc862375506
David Haas <david@Davids-MacBook-Pro.local>
2017-10-03 16:46:03 -0400

    Added compound mass

--- plugin.py
+++ plugin.py
@@ -6,11 +6,11 @@ def results(fields, original_query):
     exp = fields['~expression']
     #print exp
     try:
-        time.sleep(0.1)
+        time.sleep(0.1) # A buffer time so it doesn't display "No Results"
         ans = eval(exp)
         #print ans
     except Exception as e:
-        print e
+        # print e
         ans = 'N/A'
     return {
         "title": "{0}".format(ans)
@@ -19,20 +19,52 @@ def results(fields, original_query):
 def run(expression):
     os.system('echo "{0}"'.format(exp))
 
-def amu(element):
+def amu(compound):
+    import re
     import json
+
+    # Splits compound up based on upper case letters
     path = os.path.dirname(os.path.realpath(__file__))
+    tot_mass = 0
+    split_atoms = re.findall(r'[A-Z]+[^A-Z]*', compound)
+    elements = [no_nums(e) for e in split_atoms]
+    moles = []
+    for e in split_atoms:
+        num = re.findall('\d', e)
+        print num
+        if not num:
+            moles+=[1]
+        else:
+            moles+=[int(''.join(num))]
+
+    elements_with_moles = dict()
+    for i in range(len(elements)):
+        elements_with_moles[elements[i]] = moles[i]
+    # print split_atoms, elements_with_moles
+
     with open(path + '/periodic-data.json') as data_file:
         p_table = json.load(data_file)
-    for elem in p_table:
-        if elem['symbol'] == element:
-            mass = elem['atomicMass']
+
+    for p_elem in p_table:
+        if p_elem['symbol'] in elements_with_moles.keys():
+            num_moles = elements_with_moles[p_elem['symbol']]
+            mass = p_elem['atomicMass']
             parenthesies = mass.find('(')
             if parenthesies != -1:
-                return float(mass[0:parenthesies])
-            return float(mass)
+                mass = float(mass[0:parenthesies])
+            else:
+                mass = float(mass)
+            tot_mass += mass * num_moles
+    return tot_mass
+
+def no_nums(s):
+    new_str = s
+    for c in s:
+        if c.isdigit():
+            new_str = new_str.replace(c,'')
+    return new_str
 
 #print eval('amu(\'Hg\')')
-#print amu('Li')
+# print amu('C4H10')
 #inp="amu('Hg')"
 #print results({'~expression':inp} , '')
